893b24d677f78fcd4d75dcd052ef67156c2265fd
Saravanan <saran_jad@yahoo.co.in>
2024-10-11 23:01:54 +0100

    Added latest code

---
+++ src/com/design/creational/patterns/SingletonTestLatest.java
@@ -0,0 +1,12 @@
+package com.design.creational.patterns;
+
+public class SingletonTestLatest {
+	public static void main(String[] args) {
+		// Get the singleton instance
+		SingletoPatternLatest singleton1 = SingletoPatternLatest.getInstance();
+
+		// Show message
+		singleton1.showMessage(); 
+   }
+
+}
